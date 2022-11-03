d09a6c4b5b6a5b5f77b4acd6a3211448e6c9dbe9
C210040 <92075597+C210040@users.noreply.github.com>
2022-11-03 16:22:07 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        boolean x = true;
+        char moji = '駆';
+        double pi = 3.14;
+        long num = 314159265853979L;
+        String text = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+
+    }
+    
+}
